035f2ee6e747273fe66a7967ff7d1cad096e8888
Leophadic <leophadic12gmail.com>
2023-06-15 05:39:50 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
 int main(void)
 {
 printf("seize of a char: %d byte(s)", seizeof(char));
-printf("seize of an int %d byte(s)", seizeof(int));
-printf("seize of a long %d byte(s)", seizeof(long int));
-printf("seize of a long long int %d byte(s)", seizeof(long long int));
-printf("seize of a float %d byte(s)", seizeof(float));
+printf("seize of an int: %d byte(s)", seizeof(int));
+printf("seize of a long: %d byte(s)", seizeof(long int));
+printf("seize of a long long int: %d byte(s)", seizeof(long long int));
+printf("seize of a float: %d byte(s)", seizeof(float));
 return (0);
 }
